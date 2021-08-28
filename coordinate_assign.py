61c7bb9aa49aa65cf8af8a364c3cd9289e62e90a
Bas Terwijn <bterwijn@gmail.com>
2021-08-28 15:09:01 +0200

    update

---
+++ coordinate_assign.py
@@ -0,0 +1,25 @@
+
+def main():
+    c1 = Coordinate(1, 2)
+    print(c1)  # (1,2)
+    c2 = Coordinate(3, 4)
+    print(c2)  # (3,4)
+
+    c3 = c1.add(c2)
+    print(c3.negative()) # (-4,-6)
+    print(c3.subtract(c2)) # (1,2)
+
+class Coordinate:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+
+    def add(self, other):
+        """ returns a new coordinate that is the sum of coordinates self and other """
+        return Coordinate(self.x + other.x, self.y + other.y)
+
+main()
---
+++ coordinate_normalize_assign_solu.py
@@ -0,0 +1,46 @@
+import math
+
+def main():
+    c1 = Coordinate(1, 2)
+    print(c1)  # (1,2)
+    c1.scale(2)
+    print(c1)  # (2,4)
+    print(c1.length()) # 4.47213595499958
+    c1.normalize()
+    print(c1) # (0.4472135954999579,0.8944271909999159)
+
+class Coordinate:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"({self.x},{self.y})"
+    
+    def __add__(self, other):
+        """ returns a new coordinate that is the sum of coordinates self and other """
+        return Coordinate(self.x + other.x, self.y + other.y)
+
+    def __neg__(self):
+        """ returns a new coordinate that is the negative of the coordinate """
+        return Coordinate(-self.x, -self.y)
+
+    def __sub__(self, other):
+        """ returns a new coordinate that is self subtracted by other """
+        return self + -other
+
+    def scale(self,scale_factor):
+        """ scales the current coordinate by 'scale_factor' """
+        self.x *= scale_factor
+        self.y *= scale_factor
+
+    def length(self):
+        """ returns the length of the coordinate (distance to origin) """
+        return math.sqrt(self.x **2 + self.y **2) # pythagoras
+
+    def normalize(self):
+        """ scales coordinate so it has length equal to 1 """
+        self.scale(1 / self.length())
+    
+main()
